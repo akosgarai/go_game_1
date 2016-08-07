ece238cd121ace18eb294e908ee818406694ac96
akosgarai <szlem01@gmail.com>
2016-08-07 18:43:32 +0200

    Add user input functionality

--- game_1.go
+++ game_1.go
@@ -9,10 +9,22 @@ import (
 )
 
 type Game struct {
-	p1    string
-	p2    string
+	p1    User
+	p2    User
 	state string
 	board [][]string
+}
+
+type User struct {
+	name     string
+	timeLeft int
+	points   int
+	t        Target
+}
+
+type Target struct {
+	row int
+	col int
 }
 
 func clearScreen() {
@@ -26,7 +38,7 @@ func userInputForm(question string) string {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print(question, "\n")
 	text, _ := reader.ReadString('\n')
-	return text
+	return strings.Replace(text, "\n", "", -1)
 }
 func initGame() Game {
 	var p1Name, p2Name string
@@ -35,34 +47,73 @@ func initGame() Game {
 	p2Name = userInputForm("Second player's name?")
 	boardRow = 10
 	boardCol = 10
-	return Game{p1Name, p2Name, "p1", initBoard(boardRow, boardCol)}
+	return Game{User{p1Name, 120, 0, Target{-1, -1}}, User{p2Name, 120, 0, Target{-1, -1}}, "p1", initBoard(boardRow, boardCol)}
 }
 
-func getUserInputs() {
-	var i int
+func drawUserInfo(game Game) {
+	//var i int
+	//var q []string
+	switch game.state {
+	case "p1":
+		fmt.Print(game.p1.name, " turn.")
+	case "p2":
+		fmt.Print(game.p2.name, " turn.")
+	case "end":
+		fmt.Print("end\n")
+	}
+}
+
+func userInputHandler(game Game) Target {
+	var i, j int
+	fmt.Print("Row: ")
 	_, err := fmt.Scanf("%d", &i)
+	if err == nil {
+		fmt.Print("Col: ")
+		_, err2 := fmt.Scanf("%d", &j)
+
+		if err2 == nil {
+			if game.board[i][j] == "_" {
+				return Target{i, j}
+			}
+		}
+	}
+	return Target{-1, -1}
 }
 
 func game_fsm(game Game) {
 	clearScreen()
+	drawMenu(game.p1, game.p2)
 	drawBoard(game.board)
+	drawUserInfo(game)
 	for game.state != "end" {
 		switch game.state {
 		case "p1":
-			changeValue(game.board, 1, 3, "X")
-			game.state = "p2"
+			t := userInputHandler(game)
+			if t.row > -1 && t.col > -1 {
+				changeValue(game.board, t.row, t.col, "X")
+				game.state = "p2"
+			}
 		case "p2":
-			changeValue(game.board, 1, 4, "O")
-			game.state = "end"
+			t := userInputHandler(game)
+			if t.row > -1 && t.col > -1 {
+				changeValue(game.board, t.row, t.col, "O")
+				game.state = "p1"
+			}
 		}
 		clearScreen()
+		drawMenu(game.p1, game.p2)
 		drawBoard(game.board)
+		drawUserInfo(game)
 	}
 }
 
 func main() {
 	game := initGame()
 	game_fsm(game)
+}
+
+func drawMenu(u1, u2 User) {
+	fmt.Print(u1.name, " ", u1.timeLeft, " ", u1.points, " ", u2.points, " ", u2.timeLeft, " ", u2.name, "\n")
 }
 
 func drawBoard(board [][]string) {
