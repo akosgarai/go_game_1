94a39b1a12c27463ddef46927e08c9a9c04f9f3d
akosgarai <szlem01@gmail.com>
2016-08-07 17:31:26 +0200

    refactor

--- game_1.go
+++ game_1.go
@@ -33,24 +33,46 @@ func initGame() Game {
 	var boardRow, boardCol int
 	p1Name = userInputForm("First player's name?")
 	p2Name = userInputForm("Second player's name?")
-	boardRow = 5
-	boardCol = 5
+	boardRow = 10
+	boardCol = 10
 	return Game{p1Name, p2Name, "p1", initBoard(boardRow, boardCol)}
+}
+
+func getUserInputs() {
+	var i int
+	_, err := fmt.Scanf("%d", &i)
+}
+
+func game_fsm(game Game) {
+	clearScreen()
+	drawBoard(game.board)
+	for game.state != "end" {
+		switch game.state {
+		case "p1":
+			changeValue(game.board, 1, 3, "X")
+			game.state = "p2"
+		case "p2":
+			changeValue(game.board, 1, 4, "O")
+			game.state = "end"
+		}
+		clearScreen()
+		drawBoard(game.board)
+	}
 }
 
 func main() {
 	game := initGame()
-	clearScreen()
-	drawBoard(game.board)
-	changeValue(game.board, 1, 3, "X")
-	clearScreen()
-	drawBoard(game.board)
+	game_fsm(game)
 }
 
 func drawBoard(board [][]string) {
 	for i := 0; i < len(board); i++ {
-		fmt.Printf("%s\n", strings.Join(board[i], " "))
+		fmt.Printf("%s | %d\n", strings.Join(board[i], " "), i)
 	}
+	for j := 0; j < len(board[0]); j++ {
+		fmt.Print(j, " ")
+	}
+	fmt.Print("\n")
 }
 
 func changeValue(board [][]string, rowIndex, colIndex int, target string) [][]string {
