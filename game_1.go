1f226fdb0f1f9a28cb99c5cb108c8943da5fb4f2
akosgarai <szlem01@gmail.com>
2016-08-08 21:31:42 +0200

    refactor + improve status messages

--- game_1.go
+++ game_1.go
@@ -47,9 +47,11 @@ func getStringUserInputAfterPrintedText(textToPrint string) string {
 	text, _ := reader.ReadString('\n')
 	return strings.Replace(text, "\n", "", -1)
 }
-func userRestartHandler() bool {
+func userRestartHandler(u User) bool {
 	var response string
-	response = getStringUserInputAfterPrintedText("New game?")
+	var statusmsg string
+	statusmsg = "Player " + u.name + " won\nNew game?"
+	response = getStringUserInputAfterPrintedText(statusmsg)
 	if response == "no" {
 		return false
 	}
@@ -68,18 +70,18 @@ func initGame() Game {
 	return Game{User{p1Name, timeDefault, 0, Target{-1, -1, -1}}, User{p2Name, timeDefault, 0, Target{-1, -1, -1}}, "p1", initBoard(numOfRows, numOfCols)}
 }
 
-func drawUserInfo(game Game) {
-	switch game.state {
+func drawUserInfo(p1, p2 User, state string) {
+	switch state {
 	case "p1":
-		fmt.Print(game.p1.name, " turn.\n")
+		fmt.Print(p1.name, " turn.\n")
 	case "p2":
-		fmt.Print(game.p2.name, " turn.\n")
+		fmt.Print(p2.name, " turn.\n")
 	case "end":
 		var winner string
-		if game.p1.points > game.p2.points {
-			winner = game.p1.name
-		} else if game.p1.points < game.p2.points {
-			winner = game.p2.name
+		if p1.points > p2.points {
+			winner = p1.name
+		} else if p1.points < p2.points {
+			winner = p2.name
 		} else {
 			winner = "Points are equal, both players are looser."
 		}
@@ -104,7 +106,7 @@ func getNumericUserInputWithPrintedText(userText string) int {
 	}
 }
 
-func userInputHandler(game Game) Target {
+func userInputHandler() Target {
 	var i, j int
 	t := time.Now().UTC().UnixNano()
 	i = getNumericUserInputWithPrintedText("Row:")
@@ -127,7 +129,7 @@ func drawScreen(game Game) {
 	clearScreen()
 	drawMenu(game.p1, game.p2)
 	drawBoard(game.board)
-	drawUserInfo(game)
+	drawUserInfo(game.p1, game.p2, game.state)
 }
 
 func game_fsm(game Game) {
@@ -135,7 +137,7 @@ func game_fsm(game Game) {
 	for game.state != "end" {
 		switch game.state {
 		case "p1":
-			t := userInputHandler(game)
+			t := userInputHandler()
 			game.p1.timeLeft = game.p1.timeLeft - t.time
 			if game.p1.timeLeft < 0 {
 				game.state = "p1_fall"
@@ -151,7 +153,7 @@ func game_fsm(game Game) {
 				}
 			}
 		case "p2":
-			t := userInputHandler(game)
+			t := userInputHandler()
 			game.p2.timeLeft = game.p2.timeLeft - t.time
 			if game.p2.timeLeft < 0 {
 				game.state = "p2_fall"
@@ -167,7 +169,7 @@ func game_fsm(game Game) {
 				}
 			}
 		case "p2_fall":
-			newGame := userRestartHandler()
+			newGame := userRestartHandler(game.p1)
 			if newGame {
 				game.p1.points = game.p1.points + 1
 				restartGame(game)
@@ -176,7 +178,7 @@ func game_fsm(game Game) {
 				game.state = "end"
 			}
 		case "p1_fall":
-			newGame := userRestartHandler()
+			newGame := userRestartHandler(game.p2)
 			if newGame {
 				game.p2.points = game.p2.points + 1
 				restartGame(game)
@@ -187,7 +189,7 @@ func game_fsm(game Game) {
 		case "p1_win":
 			game.p1.points = game.p1.points + 1
 			restartGame(game)
-			newGame := userRestartHandler()
+			newGame := userRestartHandler(game.p1)
 			if newGame {
 				game.state = "p1"
 			} else {
@@ -197,7 +199,7 @@ func game_fsm(game Game) {
 		case "p2_win":
 			game.p2.points = game.p2.points + 1
 			restartGame(game)
-			newGame := userRestartHandler()
+			newGame := userRestartHandler(game.p2)
 			if newGame {
 				game.state = "p2"
 			} else {
@@ -219,7 +221,6 @@ func isWinnerStep(t Target, b [][]string) bool {
 		case 1:
 			totalHit = 1
 			j = 1
-			//check up
 			for row-j >= 0 && b[row-j][col] == expected {
 				totalHit = totalHit + 1
 				if totalHit >= targetNum {
@@ -227,9 +228,7 @@ func isWinnerStep(t Target, b [][]string) bool {
 				}
 				j = j + 1
 			}
-			//check down
 			k = 1
-			//check up
 			for row+k < numOfRows && b[row+k][col] == expected {
 				totalHit = totalHit + 1
 				if totalHit >= targetNum {
@@ -241,7 +240,6 @@ func isWinnerStep(t Target, b [][]string) bool {
 		case 2:
 			totalHit = 1
 			j = 1
-			//check up
 			for col-j >= 0 && b[row][col-j] == expected {
 				totalHit = totalHit + 1
 				if totalHit >= targetNum {
@@ -249,9 +247,7 @@ func isWinnerStep(t Target, b [][]string) bool {
 				}
 				j = j + 1
 			}
-			//check down
 			k = 1
-			//check up
 			for col+k < numOfCols && b[row][col+k] == expected {
 				totalHit = totalHit + 1
 				if totalHit >= targetNum {
@@ -263,7 +259,6 @@ func isWinnerStep(t Target, b [][]string) bool {
 		case 3:
 			totalHit = 1
 			j = 1
-			//check up
 			for row+j < numOfRows && col+j < numOfCols && b[row+j][col+j] == expected {
 				totalHit = totalHit + 1
 				if totalHit >= targetNum {
@@ -271,9 +266,7 @@ func isWinnerStep(t Target, b [][]string) bool {
 				}
 				j = j + 1
 			}
-			//check down
 			k = 1
-			//check up
 			for row-k >= 0 && col-k >= 0 && b[row-k][col-k] == expected {
 				totalHit = totalHit + 1
 				if totalHit >= targetNum {
@@ -285,7 +278,6 @@ func isWinnerStep(t Target, b [][]string) bool {
 		case 4:
 			totalHit = 1
 			j = 1
-			//check up
 			for row-j >= 0 && col+j < numOfRows && b[row-j][col+j] == expected {
 				totalHit = totalHit + 1
 				if totalHit >= targetNum {
@@ -293,9 +285,7 @@ func isWinnerStep(t Target, b [][]string) bool {
 				}
 				j = j + 1
 			}
-			//check down
 			k = 1
-			//check up
 			for row+k < numOfRows && col-k >= 0 && b[row+k][col-k] == expected {
 				totalHit = totalHit + 1
 				if totalHit >= targetNum {
